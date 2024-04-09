e6d62d47a5d8d08a978b0ce7767fb4c555c3f73a
george <g3orrg301@gmail.com>
2024-04-09 22:05:23 +0300

    Phone styling header and hero banner

--- components/Banner/HeroBanner.tsx
+++ components/Banner/HeroBanner.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
+
 import { Button } from "../ui/button";
+import { phoneNumber } from "@/lib/utils";
 
 export default function HeroBanner() {
     return (
         <div className="bg-black">
             <div className="container grid md:grid-cols-2">
-                <div className="">
+                <div className="max-md:order-1">
                     <Image alt="Person on scooter" src='/hero_scooter.jpg' width={1400} height={700} />
                 </div>
-                <div className="text-white flex flex-col justify-center gap-12">
+                <div className="text-white flex flex-col justify-center items-start gap-12 max-md:text-center max-md:items-center max-md:pt-12">
                     <h1>Scooter renthal</h1>
                     <h4>Get your scooter today and start your career</h4>
-                    <div className="flex items-end max-lg:justify-between lg:gap-[11rem]">
-                        <h3>Let's<br /> collaborate</h3>
+                    <a href={`tel:${phoneNumber}`}>
                         <Button variant='outline' className="bg-black p-6">BOOK NOW</Button>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,32 +1,41 @@
 import Link from "next/link";
+import { MapPin, Phone } from "lucide-react";
+
 import { Separator } from "../ui/separator";
 import { Sheet, SheetClose, SheetContent, SheetTrigger } from "../ui/sheet";
 import { Button } from "../ui/button";
+import { phoneNumber } from "@/lib/utils";
 
 export default function Header() {
     return (
         <header>
             <div className="max-sm:hidden bg-black text-white">
-                <div className="container flex justify-end gap-4">
-                    <div>+40.123.456.789</div>
+                <div className="container flex justify-end gap-4 px-12">
+                    <a href={`tel:${phoneNumber}`} className="flex items-center gap-2">
+                        <Phone />
+                        <span>{phoneNumber}</span>
+                    </a>
                     <Separator orientation="vertical" className="h-full" />
-                    <div>Drumul taberei</div>
+                    <div className="flex items-center gap-2">
+                        <MapPin />
+                        <span>Drumul taberei</span>
+                    </div>
                 </div>
             </div>
             <div className="container flex justify-between">
-                <Button className="p-8" variant='link'>
+                <Button className="py-8 block sm:flex" variant='link'>
                     <Link href='/'>Everest</Link>
                 </Button>
                 <nav className="max-sm:hidden">
                     <ul className="flex">
-                        <li><Button className="p-8" variant='link'><Link href='/'>Home</Link></Button></li>
-                        <li><Button className="p-8" variant='link'><Link href='/'>Terms and conditions</Link></Button></li>
-                        <li><Button className="p-8" variant='link'><Link href='/'>Contact</Link></Button></li>
+                        <li><Button className="py-8" variant='link'><Link href='/'>Home</Link></Button></li>
+                        <li><Button className="py-8" variant='link'><Link href='/'>Terms and conditions</Link></Button></li>
+                        <li><Button className="py-8" variant='link'><Link href='/'>Contact</Link></Button></li>
                     </ul>
                 </nav>
                 <div className="sm:hidden">
                     <Sheet>
-                        <SheetTrigger>Menu</SheetTrigger>
+                        <SheetTrigger className="py-8">Menu</SheetTrigger>
                         <SheetContent>
                             <nav>
                                 <ul className="">
