7ca80d575f70c943f92194c1d5acb9357b65201f
george <g3orrg301@gmail.com>
2024-05-08 17:18:25 +0300

    Dialog product gallery close button fix

--- components/Product/ProductDetailGallery.tsx
+++ components/Product/ProductDetailGallery.tsx
@@ -10,10 +10,8 @@ import { Close } from "@radix-ui/react-dialog";
 export default function ProductDetailGallery() {
     const [isOpen, setIsOpen] = useState(false);
     const [startIndex, setStartIndex] = useState(0);
-    console.log(startIndex)
 
     const handleSetStartIndex = (i: number) => {
-        console.log('clicked');
         setStartIndex(i)
         setIsOpen(prev => !prev);
     }
@@ -29,14 +27,14 @@ export default function ProductDetailGallery() {
                     ))}
                 </div>
             </DialogTrigger>
-            <DialogContent>
+            <DialogContent className="dialog-product-gallery">
                 <Carousel
                     className="w-full"
                     opts={{
                         startIndex: startIndex
                     }}
                 >
-                    <CarouselContent>
+                    <CarouselContent className="pt-2">
                         {['/street_scooter.jpg', '/scooter.png', '/beach_scooter.jpg', '/hero_scooter.jpg'].map((el, idx) => (
                             <CarouselItem key={idx}>
                                 <Image alt="Scooter image" width={500} height={500} src={el} className="aspect-square object-cover" />
@@ -46,7 +44,9 @@ export default function ProductDetailGallery() {
 
                     <CarouselPrevious className="left-0" />
                     <CarouselNext className="right-0" />
-                    <Close onClick={() => setIsOpen(false)}>close</Close>
+                    <Close onClick={() => setIsOpen(false)} className="absolute right-0 -top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-gray-100 data-[state=open]:text-gray-500 dark:ring-offset-gray-950 dark:focus:ring-gray-300 dark:data-[state=open]:bg-gray-800 dark:data-[state=open]:text-gray-400">
+                        X
+                    </Close>
                 </Carousel>
             </DialogContent>
         </Dialog>
